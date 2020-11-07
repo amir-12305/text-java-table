5de3001952291c52c3e0ca9792f71a68ef72b10a
amirkhan <amirkhan@DESKTOP-6OLPDOG>
2020-11-07 12:06:39 +0530

    code

--- javahello/src/javahello/suhel.java
+++ javahello/src/javahello/suhel.java
@@ -3,8 +3,12 @@ package javahello;
 public class suhel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num = 9;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}	
 
-	}
-
-}
+	
